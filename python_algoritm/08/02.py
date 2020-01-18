0fa52b33237c925fef4b96c7e7e9f304f37300c1
Aleksandr <lumad@bk.ru>
2020-01-18 23:09:26 +0300

    08

--- python_algoritm/08/02.py
+++ python_algoritm/08/02.py
@@ -18,6 +18,8 @@ def path_find(graph, start):
     length = len(graph)
     is_visited = [False] * length
     cost = [float('inf')] * length
+    parent = [-1] * length
+    res = [[] for _ in range(length)]
 
     cost[start] = 0
     min_cost = 0
@@ -25,20 +27,30 @@ def path_find(graph, start):
         is_visited[start] = True
         for i, vertex in enumerate(graph[start]):
             if vertex != 0 and not is_visited[i]:
+
                 if cost[i] > vertex + cost[start]:
                     cost[i] = vertex + cost[start]
-
-
+                    parent[i] = start
         min_cost = float('inf')
         for i in range(length):
             if min_cost > cost[i] and not is_visited[i]:
                 min_cost = cost[i]
                 start = i
 
-    return result
+    for i in range(length):
+        if is_visited[i]:
+            res[i].append(i)
+            j = i
+            while parent[j] != -1:
+                res[i].append(parent[j])
+                j = parent[j]
+
+            res[i].reverse()
+    return cost, res
 
 
 s = 0
-res = path_find(gr, s)
-for l in res:
-    print(l)
+cost, res = path_find(gr, s)
+print(cost)
+for a in res:
+    print(a)
