34e213b6356158746d15e01f53487e43fdbefa78
Aleksandr <lumad@bk.ru>
2020-01-18 22:26:59 +0300

    08

--- python_algoritm/08/03.py
+++ python_algoritm/08/03.py
@@ -1,16 +1,20 @@
 '''3. Написать программу, которая обходит не взвешенный ориентированный граф без петель,
-в котором все вершины связаны, по алгоритму поиска в глубину (Depth-First Search).'''
+в котором все вершины связаны, по алгоритму поиска в глубину (Depth-First Search).
+Примечания:
+a. граф должен храниться в виде списка смежности;
+b. генерация графа выполняется в отдельной функции, которая принимает на вход число вершин.
+'''
 
-gr = {
-    '0': ['2', '3', '4'],
-    '1': ['2', '3', '6'],
-    '2': ['1', '4', '6'],
-    '3': [],
-    '4': ['5'],
-    '5': ['6'],
-    '6': ['4', '5'],
-    '7': ['5', '6']
-}
+import random as r
+def GraphGenerate(N):
+    graph = {}
+    for i in range(N):
+        graph[i] = []
+        for j in range(N):
+            if r.randint(1, 4) % 3 == 0:
+                graph[i].append(j)
+
+    return graph
 
 def dfs(graph,start, is_visited):
     if start not in is_visited:
@@ -18,6 +22,10 @@ def dfs(graph,start, is_visited):
         for n in graph[start]:
             dfs(graph, n,is_visited)
 
+gr = GraphGenerate(10)
+for key, value in gr.items():
+    print(key, value)
+
 is_visited = []
-dfs(gr, '0', is_visited)
+dfs(gr, 2, is_visited)
 print(is_visited)
